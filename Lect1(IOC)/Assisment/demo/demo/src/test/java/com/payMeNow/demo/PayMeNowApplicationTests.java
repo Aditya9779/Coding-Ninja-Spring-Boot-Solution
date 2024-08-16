9b82faaef0427231279e4c7d524b8d002f0e5480
Aditya Srivastava <154252827+Aditya9779@users.noreply.github.com>
2024-08-16 22:54:58 +0530

    Add files via upload

---
+++ Lect1(IOC)/Assisment/demo/demo/src/test/java/com/payMeNow/demo/PayMeNowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.payMeNow.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayMeNowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
