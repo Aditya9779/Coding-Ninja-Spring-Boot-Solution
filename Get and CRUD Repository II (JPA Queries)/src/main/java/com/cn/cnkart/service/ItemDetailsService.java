7f3a695ea923ee5968ab7f1c96c22edd3714f1b9
Aditya Srivastava <154252827+Aditya9779@users.noreply.github.com>
2024-10-19 20:19:12 +0530

    Add files via upload

---
+++ Get and CRUD Repository II (JPA Queries)/src/main/java/com/cn/cnkart/service/ItemDetailsService.java
@@ -0,0 +1,32 @@
+package com.cn.cnkart.service;
+
+import com.cn.cnkart.dal.ItemDetailsRepository;
+import com.cn.cnkart.dal.ItemRepository;
+import com.cn.cnkart.entity.ItemDetails;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.List;
+
+
+@Service
+public class ItemDetailsService {
+    @Autowired
+    ItemDetailsRepository itemDetailsRepository;
+
+
+    public void delete(int id) {
+        itemDetailsRepository.deleteById(id);
+    }
+
+
+    public List<ItemDetails> getItemDetailsByPrice(double price) {
+     return itemDetailsRepository.findByPriceGreaterThan(price);
+    }
+
+    public List<ItemDetails> getItemDetailsByCategory(String category) {
+       return itemDetailsRepository.findByCategoryOrderByPrice(category);
+    }
+}
