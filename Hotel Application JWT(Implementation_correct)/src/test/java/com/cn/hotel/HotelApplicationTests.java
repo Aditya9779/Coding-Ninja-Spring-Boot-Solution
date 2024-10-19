e6867511388cc56b41144c36a1f3f3041f137010
Aditya Srivastava <154252827+Aditya9779@users.noreply.github.com>
2024-10-19 20:22:44 +0530

    Add files via upload

--- Hotel Application JWT(Implementation_correct)/src/test/java/com/cn/hotel/HotelApplicationTests.java
+++ Hotel Application JWT(Implementation_correct)/src/test/java/com/cn/hotel/HotelApplicationTests.java
@@ -1,4 +1,4 @@
-package org.assisment.hotel;
+package com.cn.hotel;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class HotelApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
