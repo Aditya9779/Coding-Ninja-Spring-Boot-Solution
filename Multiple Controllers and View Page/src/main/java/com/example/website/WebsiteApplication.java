848b24e6e008cbadf878a3a212e04528013a9ff6
Aditya Srivastava <154252827+Aditya9779@users.noreply.github.com>
2024-10-19 20:23:32 +0530

    Add files via upload

--- Multiple Controllers and View Page/src/main/java/com/example/website/WebsiteApplication.java
+++ Multiple Controllers and View Page/src/main/java/com/example/website/WebsiteApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@Author(name = "Aditya Srivastava", date = "14-08-2024")
 public class WebsiteApplication {
 
     public static void main(String[] args) {
